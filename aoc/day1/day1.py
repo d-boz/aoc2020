6e14dacd70e34bb4d5c82c1614214bf284a683dc
David Bozentka <djbozentka@gmail.com>
2020-12-01 19:45:39 -0500

    Cleanup

--- aoc/day1/day1.py
+++ aoc/day1/day1.py
@@ -8,7 +8,7 @@ def add_to_expense_report(line):
     except ValueError:
         return
 
-read_file_by_newline("day1.txt", add_to_expense_report)
+io.read_file_by_newline("day1.txt", add_to_expense_report)
 
 # Calculate 2020 add & sum brute force O^2
 break_flag = False
@@ -20,4 +20,15 @@ for i in range(0, len(expense_report)):
             break
     if break_flag:
         break
-print("Finished...")
+
+# Nice brute force O^3..... Yikes
+break_flag = False
+for i in range(0, len(expense_report)):
+    for j in range(i, len(expense_report)):
+        for k in range (j, len(expense_report)):
+            if expense_report[i] + expense_report[j] +expense_report [k]== 2020:
+                print("Answer: {}".format(expense_report[i]* expense_report[j] *expense_report[k]))
+                break_flag = True
+                break
+    if break_flag:
+        break
